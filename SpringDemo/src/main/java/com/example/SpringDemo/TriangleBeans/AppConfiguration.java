b3a10271189dcab5198757478c4ee978edac4c70
tymofii.bondarchuk <timabondarcuk@gmail.com>
2023-08-18 15:36:13 +0200

    basket

--- SpringDemo/src/main/java/com/example/SpringDemo/TriangleBeans/AppConfiguration.java
+++ SpringDemo/src/main/java/com/example/SpringDemo/TriangleBeans/AppConfiguration.java
@@ -1,9 +1,17 @@
 package com.example.SpringDemo.TriangleBeans;
 
 import com.example.SpringDemo.e_commerce.basket.Basket;
+import com.example.SpringDemo.e_commerce.customer.CustomerRepository;
+import com.example.SpringDemo.e_commerce.user.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.server.WebSession;
+
+import java.security.Principal;
 
 @Configuration
 public class AppConfiguration {
@@ -21,9 +29,16 @@ public class AppConfiguration {
     public Triangle integer(){
         return new Triangle(20);
     }*/
+
     @Bean
-    @SessionScope
-    public Basket basket(){
+    @SessionScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public User user(CustomerRepository customerRepository ) {
+        return customerRepository.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName() );
+    }
+
+    @Bean
+    @SessionScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Basket basket(User user){
         return new Basket();
     }
     @Bean
--- SpringDemo/src/main/java/com/example/SpringDemo/e_commerce/basket/BasketController.java
+++ SpringDemo/src/main/java/com/example/SpringDemo/e_commerce/basket/BasketController.java
@@ -1,7 +1,9 @@
 package com.example.SpringDemo.e_commerce.basket;
 
 import com.example.SpringDemo.e_commerce.product.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.Map;
 
@@ -12,16 +14,19 @@ public class BasketController {
     //TODO differenciate the baskets between the users
     Basket basket;
 
+    @Autowired
     public BasketController (Basket basket){
         this.basket = basket;
     }
 
     @GetMapping("/users/{username}/basket")
     public Map<Product, Integer> getBasket(@PathVariable("username") String username){
+        System.out.println(RequestContextHolder.getRequestAttributes().getSessionId());
         return basket.getProductMap();
     }
     @PostMapping("/users/{username}/basket")
     public void addProductsToBasket(@PathVariable("username") String username,@RequestBody Product product,@RequestParam int amount){
+        System.out.println(RequestContextHolder.getRequestAttributes().getSessionId());
         basket.addProduct(product,amount);
     }
 
